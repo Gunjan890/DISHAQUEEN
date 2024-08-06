a3fc80fc8591201c580c81fda5ffb7f40e23101a
Gunjan <165020412+Gunjan890@users.noreply.github.com>
2024-08-07 01:20:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO", "https://github.com/Gunjan890/DISHAQUEEN", # dont Change this otherwise u get error 🧧
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
